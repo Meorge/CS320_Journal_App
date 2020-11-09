5232775ba4f560018340594b02f660f52f845460
Malcolm Anderson <malcolminyo@gmail.com>
2020-11-09 10:58:12 -0800

    Using Bootstrap for UI

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Hello } from './Hello.jsx';
 import { Info } from './Info.jsx';
 
+import 'bootstrap';
+
 export const App = () => (
   <div>
     <h1>Welcome to Meteor!</h1>
