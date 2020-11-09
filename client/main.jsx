443be6db360cec4deab147f4b5acd8fae232043a
Malcolm Anderson <malcolminyo@gmail.com>
2020-11-09 11:21:50 -0800

    Very basic routing

--- client/main.jsx
+++ client/main.jsx
@@ -2,10 +2,27 @@ import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
 import { App } from '/imports/ui/App';
+import { Info } from '/imports/ui/Info';
+
+import { FlowRouter } from 'meteor/ostrio:flow-router-extra';
 
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-Meteor.startup(() => {
-  render(<App/>, document.getElementById('react-target'));
+function renderPage(page) {
+  render(page, document.getElementById('react-target'));
+}
+
+FlowRouter.route('/', {
+  name: 'index',
+  action() {
+    renderPage(<App/>);
+  }
+});
+
+FlowRouter.route('*', {
+  action() {
+    console.log("not found 404 lol");
+    renderPage(<Info/>);
+  }
 });
--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Hello } from './Hello.jsx';
 import { Info } from './Info.jsx';
 
+
 import 'bootstrap';
 
 export const App = () => (
