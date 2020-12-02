4b74bf13e32ceb5717b2e513fe4820f2b338f203
Antonio Maniscalco <antoniomani22@gmail.com>
2020-12-01 16:27:07 -0800

    Small changes to ListEntries file

--- imports/ui/ListEntries.jsx
+++ imports/ui/ListEntries.jsx
@@ -3,12 +3,15 @@ import React, { Component } from 'react';
 import {getElementById} from "domutils";
 
 export class ListEntries extends Component {
+    constructor() {
+        super(props);
+        let userRecord = Meteor.user();
+        console.log(userRecord); //Test. This should return null because there is no logged in user at this time
+    }
     render() {
         return (
             <div>
-                <h2> Welcome to entry list page</h2>
-                <button type="button" className="btn btn-primary" onClick={alert("You do not want to click that believe me")}>you won't click this</button>
-                <button onClick={prompt("What is the seceret password", "")} type={"button"} className={"btn"}/>
+                <h2> Your Entries </h2>
             </div>
         );
     }
