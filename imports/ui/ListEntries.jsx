592868ae807025f0484d6a5080498c3c1aaf1990
Antonio Maniscalco <antoniomani22@gmail.com>
2020-12-02 22:27:59 -0800

    Small changes to list page

--- imports/ui/ListEntries.jsx
+++ imports/ui/ListEntries.jsx
@@ -3,10 +3,12 @@ import React, { Component } from 'react';
 import {getElementById} from "domutils";
 
 export class ListEntries extends Component {
-    constructor() {
+    constructor(props) {
         super(props);
-        let userRecord = Meteor.user();
-        console.log(userRecord); //Test. This should return null because there is no logged in user at this time
+        let userRecord = Meteor.user(); //might need to be this.user()
+        //.user() will pull the whole profile so need to specify only pull entries list
+        //Maybe next step should be getting profiles created with entries then finish this page?
+        console.log("User Record Returned: ", userRecord); //Test. This should return null because there is no logged in user at this time
     }
     render() {
         return (
