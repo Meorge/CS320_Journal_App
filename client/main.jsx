e9a6619d33ce42fded0afa18ef76de04d57373dd
Malcolm Anderson <malcolminyo@gmail.com>
2020-12-13 20:41:06 -0800

    small changes

--- client/main.jsx
+++ client/main.jsx
@@ -1,46 +1,75 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
-import { render } from 'react-dom';
+import { mount } from 'react-mounter';
 
 import { App } from '/imports/ui/App';
 import { NotFound } from '/imports/ui/Info';
+
+import ListPageContainer_Malcolm from '/imports/ui/ListPage_Malcolm';
 import { EditPage } from '/imports/ui/EditPage';
-import { ReadEntry } from '/imports/ui/ReadEntry';
-import { ListEntries } from '/imports/ui/ListEntries';
 
 import { LoginPage } from '/imports/ui/LoginPage';
 
 import { Container } from '/imports/ui/Container';
+import { ViewPage_Malcolm } from '/imports/ui/ViewPage_Malcolm';
 
 import { FlowRouter } from 'meteor/ostrio:flow-router-extra';
 
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function renderPage(page) {
-  render(<Container content={page} userId={Meteor.userId()} />, document.getElementById('react-target'));
-}
 
 FlowRouter.route('/', {
   name: 'index',
   action() {
-    renderPage(<App/>);
+    mount(Container, { main: <App />});
+    // renderPage(<App />);
   }
 });
 
 // TODO: Make this show the entry list page
-// 11/30/2020: Just need to get the functionality of read entries then un comment if and else satements
 FlowRouter.route('/entries', {
-  name: 'entries',
+  name: 'index',
+  // waitOn: () => Meteor.subscribe('entries.getAllForUser'),
   action() {
-    renderPage(<ListEntries/>);
-    //renderPage(<ReadEntry/>); remove later
-    /*if (Meteor.user())
-      renderPage(<App/>);
+    if (Meteor.user()) {
+      mount(Container, { main: <ListPageContainer_Malcolm />});
+      // renderPage(<ListPageContainer_Malcolm/>);
+    }
     else
-      renderPage(<readEntry/>);
-      //renderPage(<readEntry/>);
-      //FlowRouter.go('login'); */
+      FlowRouter.go('login');
+  }
+});
+
+FlowRouter.route('/new', {
+  name: 'createNew',
+  action() {
+    if (Meteor.userId()) {
+      console.log("Attempt to create a new entry");
+      Meteor.call('entries.createNew', (err, res) => {
+        console.log("something happened");
+        console.log(err, res);
+        if (res) {
+          FlowRouter.go('edit', {_id: res});
+        }
+      });
+    }
+  }
+});
+
+FlowRouter.route('/view/:_id', {
+  name: 'view',
+  action(params) {
+    console.log(`Trying to go to view page, user ID is ${Meteor.userId()}`);
+    Meteor.call('entries.isOwner', params._id, (err, res) => {
+      console.log(err, res);
+      if (err || res == false) {
+        FlowRouter.go('error');
+      } else {
+        // renderPage(<EditPage id={params._id} />);
+        mount(Container, { main: <ViewPage_Malcolm id={params._id} />});
+      }
+    }); 
   }
 });
 
@@ -48,23 +77,37 @@ FlowRouter.route('/edit/:_id', {
   name: 'edit',
   action(params) {
     console.log(`Trying to go to edit page, user ID is ${Meteor.userId()}`);
-    if (Meteor.userId())
-      renderPage(<EditPage id={params._id} />);
-    else
-      FlowRouter.go('/login');
+    Meteor.call('entries.isOwner', params._id, (err, res) => {
+      console.log(err, res);
+      if (err || res == false) {
+        FlowRouter.go('error');
+      } else {
+        // renderPage(<EditPage id={params._id} />);
+        mount(Container, { main: <EditPage id={params._id} />});
+      }
+    }); 
   }
 });
 
 FlowRouter.route('/login', {
   name: 'login',
   action() {
-    renderPage(<LoginPage />);
+    // renderPage(<LoginPage />);
+    mount(Container, { main: <LoginPage />});
   }
 });
 
 FlowRouter.route('*', {
   action() {
+    FlowRouter.go('error');
+  }
+});
+
+FlowRouter.route('/error', {
+  name: 'error',
+  action() {
     console.log("not found 404 lol");
-    renderPage(<NotFound/>);
+    // renderPage(<NotFound/>);
+    mount(Container, { main: <NotFound />});
   }
 });
